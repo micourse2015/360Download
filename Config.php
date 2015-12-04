f1e040d825624bddb3672efe0a218064a7f733a6
micourse2015 <15951931083@163.com>
2015-12-04 13:48:21 +0800

    init

---
+++ Config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kisstheraik
+ * Date: 15/12/4
+ * Time: 下午1:45
+ */
+
+return array(
+    "COOKIE_PATH"=>"",
+
+);
---
+++ Cookie.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: kisstheraik
+ * Date: 15/12/4
+ * Time: 下午1:45
+ */
+class Cookie
+{
+
+}
